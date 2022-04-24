695cf977f4bd9a63fdb273b0012b9cbe7b659a76
Rajesh3222 <sairaesh078@gmail.com>
2022-04-24 09:19:51 +0530

    ipl-dashboard

---
+++ ipl-dashboard/src/main/resources/public/precache-manifest.05f30e281d9d295f9e8750e809ebd334.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "27df03a687f748eb1ac6",
+    "url": "/static/css/main.e0a825f8.chunk.css"
+  },
+  {
+    "revision": "27df03a687f748eb1ac6",
+    "url": "/static/js/main.27df03a6.chunk.js"
+  },
+  {
+    "revision": "4422e644433f4a61be16",
+    "url": "/static/js/1.4422e644.chunk.js"
+  },
+  {
+    "revision": "bfd957dd734375eb648a",
+    "url": "/static/js/2.bfd957dd.chunk.js"
+  },
+  {
+    "revision": "9c15d78cc6e756e21791",
+    "url": "/static/js/runtime~main.9c15d78c.js"
+  },
+  {
+    "revision": "eb72eb458652ac5ec90efe66c53d9261",
+    "url": "/index.html"
+  }
+];
---
+++ ipl-dashboard/src/main/resources/public/precache-manifest.ee252cfb924343a9f1e63029ccfe4139.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "191fedbccc6e0990efbc",
+    "url": "/static/css/main.e0a825f8.chunk.css"
+  },
+  {
+    "revision": "191fedbccc6e0990efbc",
+    "url": "/static/js/main.191fedbc.chunk.js"
+  },
+  {
+    "revision": "4422e644433f4a61be16",
+    "url": "/static/js/1.4422e644.chunk.js"
+  },
+  {
+    "revision": "bfd957dd734375eb648a",
+    "url": "/static/js/2.bfd957dd.chunk.js"
+  },
+  {
+    "revision": "9c15d78cc6e756e21791",
+    "url": "/static/js/runtime~main.9c15d78c.js"
+  },
+  {
+    "revision": "3cdb20b2d5dc38108baa5945e5a877ab",
+    "url": "/index.html"
+  }
+];
